6feacfdf27b6ffd8b082a19face29644e62b44cf
Mohammad Saleh <kinitegp@gmail.com>
2025-04-06 21:05:47 +0330

    bug fix

--- linux.py
+++ linux.py
@@ -1,6 +1,15 @@
 from model import snowboydecoder
 from model.config import MODEL, SENSITIVITY
 from model.call_vosk import assis
+import os
+from subprocess import run
+
+# Grant X11 access for current user (if not already granted)
+# for fix Xlib.error.DisplayConnectionError in my debian
+try:
+    run(['xhost', '+SI:localuser:' + os.getlogin()], check=True)
+except Exception as e:
+    print(f"Failed to set xhost permissions: {e}")
 
 detector = False
 
